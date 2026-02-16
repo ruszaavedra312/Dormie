b59ac68ec3585ab1ca190c470170b4bf93935dff
Ruszel Jeaus Arandela Saavedra <ruszsaavedraa@gmail.com>
2026-02-16 13:34:00 +0800

    new UI and functionality
    Registration

---
+++ Dormie/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿window.sweetAlertHelper = {
+    showSuccess: function (title, message) {
+        Swal.fire({
+            icon: 'success',
+            title: title,
+            text: message,
+            confirmButtonColor: '#3085d6'
+        });
+    },
+
+    showError: function (title, message) {
+        Swal.fire({
+            icon: 'error',
+            title: title,
+            text: message,
+            confirmButtonColor: '#d33'
+        });
+    }
+};
